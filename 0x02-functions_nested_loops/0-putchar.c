0037259f982f958b434024021802921d1c9ff72d
Wagoory <wa8oory@gmail.com>
2023-08-15 09:14:13 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: putchar function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+        puts("_putchar");
+        putchar('\n');
+        return (0);
+}
