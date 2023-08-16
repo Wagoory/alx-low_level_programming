d055f5eec5d879c9fa7f9e2ec361d2164d470f2f
Wagoory <wa8oory@gmail.com>
2023-08-16 11:11:02 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - entry point
+ *
+ * @a: first number
+ *
+ * @b: second number
+ *
+ * return: returns new value of int a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
