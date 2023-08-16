67d5e6d6463f880d2f056c05df4bcaebf5551da4
Wagoory <wa8oory@gmail.com>
2023-08-15 19:59:59 -0700

    a

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * times_table - Entry point
+ *
+ * description: print times table
+ */
+void times_table(void)
+{
+	int num;
+	int mult;
+	int prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+		for (mult = 1; mult <= 9; mult++)
+		{
+			prod = num * mult;
+			_putchar(',');
+			_putchar(' ');
+			if (prod <= 9)
+			{
+				_putchar(' ');
+				_putchar(prod + '0');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+				_putchar(prodk % 10) + '0');
+			}
+		}
+	_putchar('\n');
+	}
+}
