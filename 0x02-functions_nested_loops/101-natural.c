3cc0266b6f25f90dda58868f0c91eae9ea6bdd97
Wagoory <wa8oory@gmail.com>
2023-08-16 18:44:14 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * description - computes sum
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 1; a < 1024; a++)
+	{
+		if ((a % 3) == 0 || (a % 5) == 0)
+		{
+			b += a;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
+}
