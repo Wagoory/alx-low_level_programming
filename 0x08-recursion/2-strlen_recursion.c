0b3f780896b31aadf1a013b0c841073de40102b3
Wagoory <wa8oory@gmail.com>
2023-08-30 12:05:20 -0700

    b

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,11 +10,10 @@ int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		return (1);
+		return (0);
 	}
 	else
 	{
-	int len = 1 +  _strlen_recursion(s + 1);
+		return (1 +  _strlen_recursion(s + 1));
 	}
-	return (len);
 }
