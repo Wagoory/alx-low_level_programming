e6ea8223dd4d9ba8c180ba59c2290f45db584922
Wagoory <wa8oory@gmail.com>
2023-09-07 11:52:52 -0700

    malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -33,11 +33,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s1[i];
 	for (j = 0; j < n; j++)
 	{
-		str[i + j] = s2[j];
+		str[i] = s2[j];
 		i++;
 	}
 
-	str[i + j] = '\0';
+	str[i] = '\0';
 	return (str);
 }
 int main(void)
