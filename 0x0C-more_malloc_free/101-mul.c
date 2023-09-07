1a2998a78a7c6ab65d348d14e6fdd4f5df623b84
Wagoory <wa8oory@gmail.com>
2023-09-07 12:43:43 -0700

    malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,14 +1,17 @@
 #include "main.h"
-#include <stdlib.h>
+#include <stdlib>
 #include <stdio.h>
 #include "init.c"
 #include "check_for_digits.c"
+#include "mul.c"
+#include "_print.c"
 
 /**
- * main - multiplies two positive numbers
- * @argc: n arguments
- * @argv: args
- * Return: int
+ * main - multiply 2 numbers
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: product error
  */
 
 int main(int argc, char *argv[])
@@ -50,62 +53,4 @@ int main(int argc, char *argv[])
 	}
 	_print(a, ln - 1);
 	return (0);
-}
-	
-	char *mul(char n, char *num, int num_index, char *dest, int dest_index)
-{
-	int j, k, mul, mulrem, add, addrem;
-
-	mulrem = addrem = 0;
-	for (j = num_index, k = dest_index; j >= 0; j--, k--)
-	{
-		mul = (n - '0') * (num[j] - '0') + mulrem;
-		mulrem = mul / 10;
-		add = (dest[k] - '0') + (mul % 10) + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	for (addrem += mulrem; k >= 0 && addrem; k--)
-	{
-		add = (dest[k] - '0') + addrem;
-		addrem = add / 10;
-		dest[k] = add % 10 + '0';
-	}
-	if (addrem)
-	{
-		return (NULL);
-	}
-	return (dest);
-}
-
-int _strlen(char *string)
-{
-	int i;
-
-	for (i = 0; string[i] != '\0'; i++)
-	{
-		;
-	}
-	return (i);
-}
-
-void _print(char *str, int l)
-{
-	int i, j;
-
-	i = j = 0;
-	while (i < l)
-	{
-		if (str[i] != '0')
-		{
-			j = 1;
-		}
-		if (j || i == l - 1)
-		{
-			putchar(str[i]);
-		}
-		i++;
-	}
-	putchar('\n');
-	free(str);
 }
---
+++ 0x0C-more_malloc_free/_strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _strlen -return string length
+ * @string: the string
+ * Return: int
+ */
+
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+	{
+		;
+	}
+	return (i);
+}
