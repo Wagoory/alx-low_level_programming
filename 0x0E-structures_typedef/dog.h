35175f8a271ddec10f14608efa2663023085f3bd
Wagoory <wa8oory@gmail.com>
2023-09-11 13:23:48 -0700

    structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - a dog struct
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ * Description: a dog struct initials and age
+ */
+
+struct dog
+{
+	char* name;
+	float age;
+	char* owner;
+};
+
+#endif
