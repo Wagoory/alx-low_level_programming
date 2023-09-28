f66bcba78a30b7b38def7b6199596ce4dcaa1896
wagoory <wa8oory@gmail.com>
2023-09-28 15:07:26 -0700

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,7 +10,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int num = 0;
 
-	while (n ^ m)
+	while ((n ^ m) > 0)
 	{
 		num += ((n ^ m) & 1);
 		(n ^ m) >>= 1;
