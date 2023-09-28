f9cfd4100df0f7cbc9b22b36e84def01007c7eac
wagoory <wa8oory@gmail.com>
2023-09-28 14:49:42 -0700

    bit manipulation

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,5 +10,6 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= (sizeof(unsigned long int) * 8))
 		return (-1);
-	*n &= ~(*n << index)
+	*n &= ~(*n << index);
+	return (1);
 }
