0233c5b6dd7c11b6bc0da1c57b6e53175ffdc433
Wagoory <wa8oory@gmail.com>
2023-10-11 11:09:12 +0400

    please work

---
+++ simple_shell/day8/exec.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+int exec(char **args)
+{
+	int status = 0;
+	pid_t id;
+
+	id = fork();
+
+	if (id == 0)
+	{
+		if (execve(args[0], args, environ) == -1)
+		{
+			perror(args[0]);
+			exit(127);
+		}
+
+	}
+	else
+	{
+		waitpid(id, &status, 0);
+	}
+	return (WEXITSTATUS(status));
+}
---
+++ simple_shell/day8/main.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+int main(void)
+{
+	char *line = NULL;
+	char **tokens = NULL;
+	int status = 0;
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(1, "$ ", 2);
+		line = read_line();
+		if (line == NULL)
+		{
+			if (isatty(STDIN_FILENO))
+				write(1, "\n", 2);
+			return (status);
+		}
+		tokens = spilt_line(line);
+
+		status = exec(tokens);
+
+		free(tokens);
+		free(line);
+	}
+
+	return (status);
+}
