b9747216bfef897074f5294be7d3e9c503d4660e
Wagoory <wa8oory@gmail.com>
2023-10-12 19:18:05 +0400

    waaaai

--- simple_shell/day8/exec.c
+++ simple_shell/day8/exec.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 
-int exec(char **args)
+/*int exec(char **args)
 {
 	int status = 0;
 	pid_t id;
@@ -22,4 +22,36 @@ int exec(char **args)
 		waitpid(id, &status, 0);
 	}
 	return (WEXITSTATUS(status));
+}*/
+int exec(char **args)
+{
+    int status = 0;
+    pid_t id;
+
+    id = fork();
+
+    if (id == 0)
+    {
+        if (execve(args[0], args, environ) == -1)
+        {
+            perror("execve");
+            exit(1); // Exit with an error code to indicate failure
+        }
+    }
+    else if (id < 0)
+    {
+        perror("fork");
+        exit(1); // Exit with an error code to indicate failure
+    }
+    else
+    {
+        waitpid(id, &status, 0);
+        if (WIFEXITED(status))
+        {
+            status = WEXITSTATUS(status);
+        }
+    }
+
+    return status;
 }
+
